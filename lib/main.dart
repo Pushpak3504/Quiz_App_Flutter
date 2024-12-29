8413662db9f77223f29daef76537bd95b29c89f7
Pushpak <pushpak3504@gmail.com>
2024-12-29 10:53:16 +0530

    Splash Screen Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:quiz/splash_screen.dart';
 
 void main() {
-  
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: SplashScreen(),
+      ),
+    ),
+  );
 }
