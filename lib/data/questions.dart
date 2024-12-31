d19f8d9b5095655cb8f69384bdd32f0b16475d10
Pushpak <pushpak3504@gmail.com>
2024-12-31 12:17:23 +0530

    Added Quesitons for Quiz in Data File

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -9,5 +9,41 @@ const questions = [
       'Blocks',
       'Functions',
     ],
-  )
+  ),
+  QuizQuestions(
+    'How many types of widgets are there in Flutter?',
+    [
+      '2',
+      '4',
+      '6',
+      '8+',
+    ],
+  ),
+  QuizQuestions(
+    'Who developed the Flutter Framework and continues to maintain it today?',
+    [
+      'Google',
+      'Facebook',
+      'Microsoft',
+      'Oracle',
+    ],
+  ),
+  QuizQuestions(
+    'Which programming language is used to build Flutter applications?',
+    [
+      'Dart',
+      'Kotlin',
+      'Swift',
+      'Java',
+    ],
+  ),
+  QuizQuestions(
+    'Which function will return the widgets attached to the screen as a root of the widget tree to be rendered on screen?',
+    [
+      'runApp()',
+      'main()',
+      'container()',
+      'root()',
+    ],
+  ),
 ];
