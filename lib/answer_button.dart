7fff9f27e8cbf5c7d0b05833dd23835babbeda51
Pushpak <pushpak3504@gmail.com>
2025-01-01 16:36:09 +0530

    Created Custom Button Widget

---
+++ lib/answer_button.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton(
+      {super.key, required this.answerText, required this.onTap});
+
+  final String answerText;
+  final void Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onTap,
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.symmetric(horizontal: 70, vertical: 10),
+        backgroundColor: const Color.fromARGB(227, 0, 49, 112),
+        foregroundColor: Colors.white,
+        shape: const BeveledRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+        ),
+      ),
+      child: Text(answerText),
+    );
+  }
+}
