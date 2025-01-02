c86fe5cc3e47ad1beb751c5840db7a713856158f
Pushpak <pushpak3504@gmail.com>
2025-01-03 00:16:36 +0530

    lato font added to question

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:quiz/answer_button.dart';
 import 'package:quiz/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -32,7 +33,7 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.text,
-              style: const TextStyle(color: Colors.white),
+              style: GoogleFonts.lato(color: Colors.white),
               textAlign: TextAlign.center,
             ),
             const SizedBox(height: 30),
